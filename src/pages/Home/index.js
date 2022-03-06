6ea9070dd4af0c7e1b2792a82f03a33b07b6f089
nctai19125120 <63503421+nctai19125120@users.noreply.github.com>
2022-03-07 04:14:32 +0700

    Dev <Header:miss border while being choose> <Footer:empty information>

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from "../../components/header";
+import Footer from "../../components/footer";
 const HomePage = (props) => {
     return (
         <div>
             <Header></Header>
             Home
+            <Footer></Footer>
         </div>
     );
 }
